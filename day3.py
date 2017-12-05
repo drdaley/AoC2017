c1836004a0889d7fec37db09ae4a41558dccf242
drdaley <mail.david.daley@gmail.com>
2017-12-05 15:57:49 -0500

    cleaned unused variables

--- day3.py
+++ day3.py
@@ -4,5 +4,5 @@ inp = float(sys.argv[1]) # get input
 n = ma.ceil((ma.sqrt(inp)-1)/2) # number of 'rings' ourwards where [ (1+2n)^2 >= inp ] for the lowest positive n
 r_max = (2*n+1)**2 # highest number in the 'ring'
 ring = r_max - inp # number of spces on the ring from the highest value to given value
-m = abs(ring%(2*n)-n) #The number of steps along the ring 
+m = abs(ring%(2*n)-n) #The number of steps along the ring
 print('The solution to part 1 is {0}'.format(n+m))
