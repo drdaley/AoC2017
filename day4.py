3911b0b5dc4521d39f2b9a072c657f63b2a80400
drdaley <mail.david.daley@gmail.com>
2017-12-06 17:54:40 -0500

    part 1 works

--- day4.py
+++ day4.py
@@ -1,5 +1,7 @@
-def pass_test(phrase):
+def pass_test(phrase): # given a phrase, returns true if the phrase passes, else false
+	phrase = phrase.rstrip('\n')
 	p_list = phrase.split(' ')
+	print(p_list)
 	pf = list()
 	for w in p_list:
 		a = p_list.count(w)
@@ -8,3 +10,18 @@ def pass_test(phrase):
 	if len(pf) < len(p_list) : print('pass/fail list error')
 	if all(pf) : return True
 	else: return False
+
+f = open('day4_inp.txt')
+inp = f.readlines()
+f.close()
+
+valid = list()
+
+for phrase in inp:
+	print(phrase)
+	print(pass_test(phrase))
+	valid.append(pass_test(phrase))
+
+print(len(inp))
+print(len(valid))
+print(sum(valid))
