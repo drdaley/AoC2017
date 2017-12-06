804fef3e954177fa8bfd81c18b055a9d17dfa999
drdaley <mail.david.daley@gmail.com>
2017-12-06 15:02:26 -0500

    broke part 1 somewhere along the way, part 2 gives a solution

--- day3.py
+++ day3.py
@@ -10,31 +10,54 @@ m = abs(ring%(2*n)-n) #The number of steps along the ring
 # begin part 2 
 def xy_coord(steps,n): #given a number of steps around a ring and the 'n' value of the ring, gives a tuple (x,y)
 	if steps <= 2*n: return (n, -n+steps)
-	elif steps > 2*n and steps <= 4*n : return (n-(steps-2*n) , n)
+	elif steps > 2*n and steps <= 4*n : return (int(n-(steps-2*n)) , n)
 	elif steps > 4*n and steps <= 6*n : return (-n,n-(steps-4*n))
-	elif steps > 6*n : return (-n+(steps-6*n)),-n)
+	elif steps > 6*n : return (-n+(steps-6*n),-n)
 	else: print('xy_coord sanity fail')
 
+def gen_adjacent(xy):
+	hits = list()
+	pm1 = list(range(-1,2))
+	#print(xy)
+	for n in pm1:
+		for m in pm1:
+			#print(xy)
+			#print((xy[0]+m,xy[1]+n))
+			if (xy[0]+m,xy[1]+n) != xy : hits.append((xy[0]+m,xy[1]+n))
+	return hits
 	
-vals = dict()
-vals[(0,0)] = 1
+def find_val(grid, adj):
+	vals = list()
+	for a in adj:
+		if a in grid.keys(): vals.append(grid[a])
+		else:pass
+	return sum(vals)
+
+grid = dict()
+grid[(0,0)] = 1
 n=0
-while n <=5:
+found = False
+while True: 
+	if found == True : break
+	else: pass
 	n=n+1
 	circ = 8*n
 	steps = 0
 	while steps <= circ :
 		steps= steps+1
-		print(xy_coord(steps,n))
-		vals[xy_coord(steps,n)] = 1
-		
-	
+		print(gen_adjacent(xy_coord(steps,n)))
+		grid[xy_coord(steps,n)] = find_val(grid, gen_adjacent(xy_coord(steps,n))) # Hmmmmmm
+		if grid[xy_coord(steps,n)] >= inp : 
+			found = True
+			solution = grid[xy_coord(steps,n)]
+			break
 #def y_coord(steps,n):
 #	if steps <= 2*n: return -n+steps
 #	elif steps > 2*n and steps <= 4*n : return n
 #	elif steps > 4*n and steps <= 6*n : return (n-(steps%(2*n)))
 #	elif steps > 6*n : return -n
 #	else: print('y_coord sanity fail')
-	
+#print(grid)
 
 print('The solution to part 1 is {0}'.format(n+m))
+print(solution)
